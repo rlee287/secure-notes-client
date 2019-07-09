50c91d9fb5b0fcf84d7849160ecaf1a34d51bdb7
rlee287 <rlee287@yahoo.com>
2019-07-09 11:04:21 -0700

    Add generic async_run_await_result method to thread pool

--- secure_notes_client/thread_pool.py
+++ secure_notes_client/thread_pool.py
@@ -1,4 +1,7 @@
 from concurrent.futures import ThreadPoolExecutor
+import time
+
+from PySide2.QtCore import QCoreApplication
 
 thread_pool=None
 
@@ -13,3 +16,12 @@ def deinit_thread_pool():
 def submit_task(function,*args,**kwargs):
     global thread_pool
     return thread_pool.submit(function,*args,**kwargs)
+
+#TODO: find a less hacky way to keep events processed
+def async_run_await_result(function,*args,**kwargs):
+    future_obj=thread_pool.submit(function,*args,**kwargs)
+    while not future_obj.done():
+        QCoreApplication.processEvents()
+        time.sleep(0.1)
+    future_result=future_obj.result()
+    return future_result
