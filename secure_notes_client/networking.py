65e0caa8a3a7f50a0b7f7b5febdbf66f201bd0e3
rlee287 <rlee287@yahoo.com>
2019-07-09 11:08:02 -0700

    Update networking to use async_run_await_result

--- secure_notes_client/networking.py
+++ secure_notes_client/networking.py
@@ -1,24 +1,17 @@
-from PySide2.QtCore import QCoreApplication
-
 import requests
 from urllib.parse import urljoin
+import posixpath # Url slashes match POSIX directory separators
 import thread_pool
-import time
 
 def construct_token_header(token):
     request_header={"Authorization":"Bearer "+token}
     return request_header
 
-#TODO: find a less hacky way to get intermediate dialogs to show
 def get_login_token(base_url,username,password):
     login_url=urljoin(base_url,"login")
-    print(login_url)
     try:
-        login_future=thread_pool.submit_task(requests.post,
+        login_request=thread_pool.async_run_await_result(requests.post,
                 login_url,auth=(username,password))
-        while not login_future.done():
-            QCoreApplication.processEvents()
-        login_request=login_future.result()
         if login_request.status_code!=200:
             return None
         return login_request.json()["token"]
@@ -27,14 +20,10 @@ def get_login_token(base_url,username,password):
 
 def send_logout_request(base_url,token):
     logout_url=urljoin(base_url,"logout")
-    print(logout_url)
+    logout_headers=construct_token_header(token)
     try:
-        logout_headers=construct_token_header(token)
-        logout_future=thread_pool.submit_task(requests.post,
+        logout_request=thread_pool.async_run_await_result(requests.post,
                 logout_url,headers=logout_headers)
-        while not logout_future.done():
-            QCoreApplication.processEvents()
-        logout_request=logout_future.result()
         if logout_request.status_code!=204:
             return False
         return True
