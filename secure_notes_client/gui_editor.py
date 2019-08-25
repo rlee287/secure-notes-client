5f731efaf0992a7977bea77b52b3110a9030f46c
rlee287 <rlee287@yahoo.com>
2019-08-25 14:45:31 -0400

    Continue adjusting note edit UI

--- secure_notes_client/gui_editor.py
+++ secure_notes_client/gui_editor.py
@@ -10,7 +10,24 @@ class EditorClass(QtWidgets.QMainWindow):
         self.ui_obj = ui_obj
         self.config_obj = config_obj
         self.note_id = note_id
+        self.edited_since_last_save = False
+        self.note_obj=filesystem.read_noteobj(config_obj,note_id)
+
         ui_obj.setupUi(self)
-        note_obj=filesystem.read_noteobj(config_obj,note_id)
-        self.setWindowTitle(note_obj["note"]["title"])
-        ui_obj.noteTextEdit.setPlainText(note_obj["note"]["text"])
+
+        self.setWindowTitle(self.note_obj["note"]["title"])
+        ui_obj.titleLineEdit.setText(self.note_obj["note"]["title"])
+        ui_obj.noteTextEdit.setPlainText(self.note_obj["note"]["text"])
+
+        ui_obj.titleLineEdit.editingFinished.connect(self.mark_edited)
+        ui_obj.noteTextEdit.textChanged.connect(self.mark_edited)
+        ui_obj.actionSave.triggered.connect(self.save_file)
+
+    def mark_edited(self):
+        self.edited_since_last_save = True
+    
+    def save_file(self):
+        note_obj_copy = self.note_obj.copy()
+        note_obj_copy["note"]["title"] = ui_obj.titleLineEdit.getText()
+        note_obj_copy["note"]["text"] = ui_obj.noteTextEdit.getText()
+        self.edited_since_last_save = False
